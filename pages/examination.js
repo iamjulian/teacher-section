f25c3a934b621c5cdcd8ed3c79ac500129a96d45
julian-of-21twelve-interactive <julian@21twelveinteractive.com>
2021-09-12 22:59:46 +0530

    added examination links

--- pages/examination.js
+++ pages/examination.js
@@ -1,31 +1,28 @@
 import { Row, Col, Input, Button, notification } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 import styled from "styled-components";
 
-const links = [];
+const links = [
+  {
+    class: "1A",
+    link: "https://forms.gle/bDGpfKEYL4xLCamv7",
+  },
+  {
+    class: "1B",
+    link: "https://forms.gle/4qguuUAKbrYjRgPZ6",
+  },
+  {
+    class: "1C",
+    link: "https://forms.gle/uep8yUdo4rA9WWDQ8",
+  },
+  {
+    class: "1D",
+    link: "https://forms.gle/af6hgBgxK3AAj7ox7",
+  },
+];
 
 const examination = () => {
-  const [links, setLinks] = useState([
-    {
-      class: "1A",
-      link: "https://forms.gle/bDGpfKEYL4xLCamv7",
-    },
-    {
-      class: "1B",
-      link: "https://forms.gle/4qguuUAKbrYjRgPZ6",
-    },
-    {
-      class: "1C",
-      link: "https://forms.gle/uep8yUdo4rA9WWDQ8",
-    },
-    {
-      class: "1D",
-      link: "https://forms.gle/af6hgBgxK3AAj7ox7",
-    },
-  ]);
-
   const openNotificationWithIcon = (placement) => {
     notification.success({
       message: "Link Coppied",
